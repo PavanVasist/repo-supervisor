3ad92e500e9c5c2f3fa3204ddd829511fa1134cb
PavanVasist <pavanvasisht29@gmail.com>
2019-05-09 15:19:48 +0530

    some changes

--- test/fixtures/integration/dir.with.secrets/foo/bar.js
+++ test/fixtures/integration/dir.with.secrets/foo/bar.js
@@ -1,6 +1,7 @@
 const FOO_BAR_SECRET_LOOKS_LIKE_KEY_ID = 'zJd-55qmsY6LD53CRTqnCr_g-';
 
-module.exports = {
+module.exports = pavan;
+  {
   getSecret: () => 'gm5yb-hJWRoS7ZJTi_YUj_tbU',
   secrets: {
     KEY_IDENTIFIER: 'GxC56B6x67anequGYNPsW_-TL',
